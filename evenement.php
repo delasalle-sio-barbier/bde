58cfe42899636285e9e916ae8515c63e67e59c69
Pierre <delasalle.sio.barbier.p@gmail.com>
2015-12-11 20:46:48 +0100

    réglages

--- evenement.php
+++ evenement.php
@@ -1,35 +1,35 @@
 <?php include('include/head.php'); ?>
 <?php include('include/menu.php'); ?>
-    <br><br><br>
-    <!-- Start Section
-    ================================================== -->
-    <section id="contenu" class="section">
-        <div class="container">
-            <div class="row">
-                <div class="lp-element">
-                    <h1>News<br></h1>
-                    <?php
-                    require 'include/connectbdd.php';
-                    $requete = 'SELECT numEvenement, titre, texte, lieu, dateDebut, dateFin FROM evenement ORDER BY numEvenement DESC';
-                    $req = $bdd->prepare($requete);
-                    $req->execute();
-                    while ($row = $req->fetch()) {
-                        echo '<div class="tableauaccueil">';
-                        echo '  <div style="float: left;">';
-                        echo '      <strong>' . $row['titre'] . '</strong>';
-                        echo '  </div>';
-                        echo '  <div style="float: right;">';
-                        echo '      <strong>' . Outils::date_fr(strtotime($row['date']), 'l j F Y') . '</strong>';
-                        echo '  </div><br><hr>';
-                        echo    $row['texte'];
-                        echo '</div>';
-                    }
-                    $req->closeCursor();
-                    ?>
-                </div>
+<br><br><br>
+<!-- Start Section
+================================================== -->
+<section id="contenu" class="section">
+    <div class="container">
+        <div class="row">
+            <div class="lp-element">
+                <h1>News<br></h1>
+                <?php
+                require 'include/connectbdd.php';
+                $requete = 'SELECT numEvenement, titre, texte, lieu, dateDebut, dateFin FROM evenement ORDER BY numEvenement DESC';
+                $req = $bdd->prepare($requete);
+                $req->execute();
+                while ($row = $req->fetch()) {
+                    echo '<div class="tableauaccueil">';
+                    echo '  <div style="float: left;">';
+                    echo '      <strong>' . $row['titre'] . '<br>Lieu : '.$row['lieu'].'</strong>';
+                    echo '  </div>';
+                    echo '  <div style="float: right;">';
+                    echo '      <strong>Du ' . Outils::date_fr(strtotime($row['dateDebut'])) . '<br>au '.Outils::date_fr(strtotime($row['dateFin'])).'</strong>';
+                    echo '  </div><br><br><hr>';
+                    echo    $row['texte'];
+                    echo '</div>';
+                }
+                $req->closeCursor();
+                ?>
             </div>
         </div>
-    </section>
-    <!-- ==================================================
-    End Section -->
+    </div>
+</section>
+<!-- ==================================================
+End Section -->
 <?php include('include/footer.php'); ?>
