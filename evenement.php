f09fb426f9d1b9b84886c72a48e4f1da6dbb86df
delasalle-sio-deoliveira <delasalle.sio.deoliveira.n@gmail.com>
2015-12-11 17:16:54 +0100

    page evenement

--- evenement.php
+++ evenement.php
@@ -1,7 +1,35 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: deolivei.n
- * Date: 11/12/2015
- * Time: 14:02
- */
+<?php include('include/head.php'); ?>
+<?php include('include/menu.php'); ?>
+    <br><br><br>
+    <!-- Start Section
+    ================================================== -->
+    <section id="contenu" class="section">
+        <div class="container">
+            <div class="row">
+                <div class="lp-element">
+                    <h1>News<br></h1>
+                    <?php
+                    require 'include/connectbdd.php';
+                    $requete = 'SELECT numEvenement, titre, texte, lieu, dateDebut, dateFin FROM evenement';
+                    $req = $bdd->prepare($requete);
+                    $req->execute();
+                    while ($row = $req->fetch()) {
+                        echo '<div class="tableauaccueil">';
+                        echo '  <div style="float: left;">';
+                        echo '      <strong>' . $row['titre'] . '</strong>';
+                        echo '  </div>';
+                        echo '  <div style="float: right;">';
+                        echo '      <strong>' . Outils::date_fr(strtotime($row['date']), 'l j F Y') . '</strong>';
+                        echo '  </div><br><hr>';
+                        echo    $row['texte'];
+                        echo '</div>';
+                    }
+                    $req->closeCursor();
+                    ?>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ==================================================
+    End Section -->
+<?php include('include/footer.php'); ?>
