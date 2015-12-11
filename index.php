4f0a93d906294dccf9b6d6daa11ceffe62558b68
delasalle-sio-barbier <delasalle.sio.barbier.p@gmail.com>
2015-12-11 15:44:26 +0100

    Tableaux news et agenda par BDD sur la page d'accueil

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
                         $req->execute();
                         $row = $req->fetch();
                         echo $row["texte"];
-                        $req->closeCursor();
                         ?>
                     </p>
                 </div>
@@ -36,71 +35,46 @@ End Hero -->
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <h1>News<br></h1>
-                <div class="tableaunewsaccueil">
-                    <div style="float: left;">
-                        <strong>Titre</strong>
-                    </div>
-                    <div style="float: right;">
-                        <strong>Date de la news</strong>
-                    </div>
-                    <br><hr>
-                    Texte
-                </div>
-                <div class="tableaunewsaccueil">
-                    <div style="float: left;">
-                        <strong>Titre</strong>
-                    </div>
-                    <div style="float: right;">
-                        <strong>Date de la news</strong>
-                    </div>
-                    <br><hr>
-                    Texte
-                </div>
-                <div class="tableaunewsaccueil">
-                    <div style="float: left;">
-                        <strong>Titre</strong>
-                    </div>
-                    <div style="float: right;">
-                        <strong>Date de la news</strong>
-                    </div>
-                    <br><hr>
-                    Texte
+                <div class="lp-element">
+                    <h1>News<br></h1>
+                    <?php
+                    $requete = 'SELECT numNews, titre, texte, date FROM news LIMIT 3';
+                    $req = $bdd->prepare($requete);
+                    $req->execute();
+                    while ($row = $req->fetch()) {
+                        echo '<div class="tableauaccueil">';
+                        echo '  <div style="float: left;">';
+                        echo '      <strong>' . $row['titre'] . '</strong>';
+                        echo '  </div>';
+                        echo '  <div style="float: right;">';
+                        echo '      <strong>' . $row['date'] . '</strong>';
+                        echo '  </div><br><hr>';
+                        echo    $row['texte'];
+                        echo '</div>';
+                    }
+                    ?>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="lp-element">
                     <h1>Agenda<br></h1>
-                    <div class="tableaunewsaccueil">
-                        <div style="float: left;">
-                            <strong>Titre</strong>
-                        </div>
-                        <div style="float: right;">
-                            <strong>Date de la news</strong>
-                        </div>
-                        <br><hr>
-                        Texte
-                    </div>
-                    <div class="tableaunewsaccueil">
-                        <div style="float: left;">
-                            <strong>Titre</strong>
-                        </div>
-                        <div style="float: right;">
-                            <strong>Date de la news</strong>
-                        </div>
-                        <br><hr>
-                        Texte
-                    </div>
-                    <div class="tableaunewsaccueil">
-                        <div style="float: left;">
-                            <strong>Titre</strong>
-                        </div>
-                        <div style="float: right;">
-                            <strong>Date de la news</strong>
-                        </div>
-                        <br><hr>
-                        Texte
-                    </div>
+                    <?php
+                    $requete = 'SELECT numEvenement, titre, texte, lieu, dateDebut, dateFin FROM evenement LIMIT 3';
+                    $req = $bdd->prepare($requete);
+                    $req->execute();
+                    while ($row = $req->fetch()) {
+                        echo '<div class="tableauaccueil">';
+                        echo '  <div style="float: left;">';
+                        echo '      <strong>' . $row['titre'] . '<br>Lieu : '.$row['lieu'].'</strong>';
+                        echo '  </div>';
+                        echo '  <div style="float: right;">';
+                        echo '      <strong>Du ' . $row['dateDebut'] . '<br>au '.$row['dateFin'].'</strong>';
+                        echo '  </div><br><br><hr>';
+                        echo    $row['texte'];
+                        echo '</div>';
+                    }
+                    $req->closeCursor();
+                    ?>
                 </div>
             </div>
         </div>
