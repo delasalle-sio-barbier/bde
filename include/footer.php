ede62ccfd69aa5f33250bf2b5bd767a2d7eb5514
delasalle-sio-deoliveira <delasalle.sio.deoliveira.n@gmail.com>
2015-12-11 15:10:39 +0100

    azrrz

--- include/footer.php
+++ include/footer.php
@@ -6,7 +6,7 @@
                 <div class="col-md-4">
                     <div class="lp-element">
                         <a href="https://www.facebook.com/bde.lycee.dls/" target="_blank"><img src="style/images/fb_icone.png" alt="facebook" width="50" height="50" border="0" /></a>
-                        <a href="https://www.facebook.com/bde.lycee.dls/" target="_blank"><img src="style/images/tw_icone.png" alt="twitter" width="50" height="50" border="0" /></a>
+                        <a href="https://www.twitter.com/bde.lycee.dls/" target="_blank"><img src="style/images/tw_icone.png" alt="twitter" width="50" height="50" border="0" /></a><br>
                         <!-- PAS DE PAGE TWITTER -->
 
                         BDE<br>
@@ -21,6 +21,7 @@
                 </div>
                 <div class="col-md-4">
                     <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="400" height="350" src="https://maps.google.com/maps?hl=en&q=5 Rue de la Motte Brûlon&ie=UTF8&t=k&z=17&iwloc=B&output=embed"><div><small><a href="http://embedgooglemaps.com">embedgooglemaps.com</a></small></div><div><small><a href="http://www.ultimatewebtraffic.com">Buy website traffic</a></small></div></iframe>
+                    <!-- iframe : googleMaps -->
                 </div>
             </div>
         </div>
