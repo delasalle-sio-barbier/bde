d649a503efe45d1158f45d638a74b2d9ea6323de
delasalle-sio-barbier <vovaxi35>
2016-02-25 15:18:22 +0100

    modification panel

---
+++ membre/gestionevenement.php
@@ -0,0 +1,22 @@
+<?php include('../include/head.php'); ?>
+<?php include('../include/menu.php'); ?>
+    <div class="container">
+        <?php
+        /* si le membre est connecte*/
+        if(isset($_SESSION['numMembre']) && ($_SESSION['privilege'] == 'admin' || $_SESSION['privilege'] == 'owner'))
+        {
+            echo '<h1>Gestion des évenements</h1><hr>';?>
+            <a href="newevenement.php" style="text-decoration : underline;">Créer un nouvel évenement</a>
+            <?php
+        }
+        else
+        {
+            ?>
+            <div class="panel panel-danger">
+                <div class="panel-heading">Erreur : Vous n'avez pas l'accès à cette page. Vous n'êtes pas admin ou vous n'êtes pas connectés !</div>
+            </div>
+            <?php
+        }
+        ?>
+    </div> <!-- /container -->
+<?php include('../include/footer.php'); ?>
---
+++ membre/gestionnews.php
@@ -0,0 +1,22 @@
+<?php include('../include/head.php'); ?>
+<?php include('../include/menu.php'); ?>
+    <div class="container">
+        <?php
+        /* si le membre est connecte*/
+        if(isset($_SESSION['numMembre']) && ($_SESSION['privilege'] == 'admin' || $_SESSION['privilege'] == 'owner'))
+        {
+            echo '<h1>Gestion des news</h1><hr>';?>
+            <a href="newnews.php" style="text-decoration : underline;">Créer une nouvelle news</a>
+            <?php
+        }
+        else
+        {
+            ?>
+            <div class="panel panel-danger">
+                <div class="panel-heading">Erreur : Vous n'avez pas l'accès à cette page. Vous n'êtes pas admin ou vous n'êtes pas connectés !</div>
+            </div>
+            <?php
+        }
+        ?>
+    </div> <!-- /container -->
+<?php include('../include/footer.php'); ?>
